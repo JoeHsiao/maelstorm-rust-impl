a7399029ba880d80a2cfc8af836cf28c2a91abea
Joe Hsiao <joe.phsiao@gmail.com>
2025-09-25 15:41:45 -0500

    Separate message data into lib

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,36 +1,6 @@
 use anyhow::{Result, bail};
-use serde::{Deserialize, Serialize};
 use std::io::BufRead;
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Message {
-    pub src: String,
-    #[serde(rename = "dest")]
-    pub dst: String,
-    pub body: Body,
-}
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "type")]
-#[serde(rename_all = "snake_case")]
-pub enum Body {
-    Echo {
-        msg_id: u64,
-        echo: String,
-    },
-    EchoOk {
-        msg_id: u64,
-        in_reply_to: u64,
-        echo: String,
-    },
-    Init {
-        msg_id: u64,
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk {
-        in_reply_to: u64,
-    },
-}
+use maelstrom_rust_impl::{Message, Body};
 struct Echoer {
     node_id: String,
     incr_id: u64,
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message {
+    pub src: String,
+    #[serde(rename = "dest")]
+    pub dst: String,
+    pub body: Body,
+}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum Body {
+    Echo {
+        msg_id: u64,
+        echo: String,
+    },
+    EchoOk {
+        msg_id: u64,
+        in_reply_to: u64,
+        echo: String,
+    },
+    Init {
+        msg_id: u64,
+        node_id: String,
+        node_ids: Vec<String>,
+    },
+    InitOk {
+        in_reply_to: u64,
+    },
+}
