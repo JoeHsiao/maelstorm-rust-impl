e01cc821900ac4f796a06c7f18dd549617aec498
Joe Hsiao <joe.phsiao@gmail.com>
2025-10-02 17:27:07 -0500

    Separate normal messages and the do_gossip command into two types

--- src/bin/broadcast.rs
+++ src/bin/broadcast.rs
@@ -1,5 +1,7 @@
-use anyhow::Result;
-use maelstrom_rust_impl::{Body, MaelstromNode, MaelstromNodeActions, MaelstromNodeId, Message};
+use anyhow::{Context, Result};
+use maelstrom_rust_impl::{
+    Body, Event, MaelstromNode, MaelstromNodeActions, MaelstromNodeId, Message,
+};
 use std::collections::{HashMap, HashSet};
 use std::thread;
 use std::thread::sleep;
@@ -24,96 +26,108 @@ fn main() -> Result<()> {
         },
     };
 
-    broadcaster.handle("gossip", |node, msg: Message| {
-        if let Body::Gossip { message } = msg.body {
-            node.extra_data.seen_messages.extend(message);
+    broadcaster.handle("gossip", |node, event: Event| {
+        if let Event::Message(msg) = event {
+            if let Body::Gossip { message } = msg.body {
+                node.extra_data.seen_messages.extend(message);
+            };
+            return None;
+        }
+        None
+    });
+
+    broadcaster.handle("do_gossip", |node, event: Event| {
+        if let Event::Action(name) = event {
+            if name == "do_gossip" {
+                let node_id = node.node_id.as_str().expect("node_id is not assigned");
+                let neighbors = node
+                    .extra_data
+                    .topology
+                    .clone()
+                    .remove(&node_id)
+                    .expect(&format!("topology missing entry for node id {}", node_id));
+                neighbors.iter().for_each(|n| {
+                    let gossip = Message {
+                        src: node_id.clone(),
+                        dst: n.clone(),
+                        body: Body::Gossip {
+                            message: node.extra_data.seen_messages.clone(),
+                        },
+                    };
+                    let _ = node.send(gossip);
+                })
+            }
+            return None;
+        }
+        None
+    });
+
+    broadcaster.handle("broadcast", |node, event: Event| {
+        if let Event::Message(msg) = event {
+            if let Body::Broadcast { msg_id, message } = msg.clone().body {
+                let node_id = node.node_id.as_str().expect("node id is not assigned");
+                node.extra_data.seen_messages.insert(message);
+                return Some(Ok(Message {
+                    src: node_id.clone(),
+                    dst: msg.src,
+                    body: Body::BroadcastOk {
+                        in_reply_to: msg_id,
+                    },
+                }));
+            } else {
+                return None;
+            }
+        }
+        None
+    });
+
+    broadcaster.handle("topology", |node, event: Event| {
+        if let Event::Message(msg) = event {
+            if let Body::Topology { msg_id, topology } = msg.body {
+                node.extra_data.topology = topology;
+                return Some(Ok(Message {
+                    src: node.node_id.as_str().expect("node id is not assigned"),
+                    dst: msg.src,
+                    body: Body::TopologyOk {
+                        in_reply_to: msg_id,
+                    },
+                }));
+            } else {
+                return None;
+            }
+        }
+        None
+    });
+    broadcaster.handle("read", |node, event: Event| {
+        if let Event::Message(msg) = event {
+            if let Body::Read { msg_id } = msg.body {
+                return Some(Ok(Message {
+                    src: node.node_id.as_str().expect("node id is not assigned"),
+                    dst: msg.src,
+                    body: Body::ReadOk {
+                        in_reply_to: msg_id,
+                        messages: node.extra_data.seen_messages.clone(),
+                    },
+                }));
+            } else {
+                return None;
+            }
         };
         None
     });
-    broadcaster.handle("do_gossip", |node, msg: Message| {
-        if let Body::DoGossip = msg.body {
-            let node_id = node.node_id.as_str().expect("node_id is not assigned");
-            let neighbors = node
-                .extra_data
-                .topology
-                .clone()
-                .remove(&node_id)
-                .expect(&format!("topology missing entry for node id {}", node_id));
-            neighbors.iter().for_each(|n| {
-                let gossip = Message {
-                    src: node_id.clone(),
-                    dst: n.clone(),
-                    body: Body::Gossip {
-                        message: node.extra_data.seen_messages.clone(),
-                    },
-                };
-                let _ = node.send(gossip);
-            })
-        }
-        None
-    });
 
-    broadcaster.handle("broadcast", |node, msg: Message| {
-        if let Body::Broadcast { msg_id, message } = msg.clone().body {
-            let node_id = node.node_id.as_str().expect("node id is not assigned");
-            node.extra_data.seen_messages.insert(message);
-            Some(Ok(Message {
-                src: node_id.clone(),
-                dst: msg.src,
-                body: Body::BroadcastOk {
-                    in_reply_to: msg_id,
-                },
-            }))
-        } else {
-            None
+    let (sender, receiver) = std::sync::mpsc::channel();
+    thread::spawn({
+        let sender = sender.clone();
+        move || -> Result<()> {
+            loop {
+                sleep(Duration::from_millis(300));
+                sender
+                    .send(Event::Action("do_gossip".into()))
+                    .context("Cannot send do_gossip")?;
+            }
         }
     });
-
-    broadcaster.handle("topology", |node, msg: Message| {
-        if let Body::Topology { msg_id, topology } = msg.body {
-            node.extra_data.topology = topology;
-
-            thread::spawn({
-                let node_id = node.node_id.as_str().expect("node id is not assigned");
-                move || {
-                    loop {
-                        sleep(Duration::from_millis(300));
-                        let gossip_reminder = Message {
-                            src: node_id.clone(),
-                            dst: node_id.clone(),
-                            body: Body::DoGossip,
-                        };
-                        println!("{}", serde_json::to_string(&gossip_reminder).expect("Failed to serialize gossip reminder"));
-                    }
-                }
-            });
-
-            Some(Ok(Message {
-                src: node.node_id.as_str().expect("node id is not assigned"),
-                dst: msg.src,
-                body: Body::TopologyOk {
-                    in_reply_to: msg_id,
-                },
-            }))
-        } else {
-            None
-        }
-    });
-    broadcaster.handle("read", |node, msg: Message| {
-        if let Body::Read { msg_id } = msg.body {
-            Some(Ok(Message {
-                src: node.node_id.as_str().expect("node id is not assigned"),
-                dst: msg.src,
-                body: Body::ReadOk {
-                    in_reply_to: msg_id,
-                    messages: node.extra_data.seen_messages.clone(),
-                },
-            }))
-        } else {
-            None
-        }
-    });
-
-    broadcaster.run()?;
+    broadcaster.run(sender, receiver)?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
 use std::io::BufRead;
 use std::rc::Rc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Message {
@@ -60,7 +62,6 @@ pub enum Body {
         in_reply_to: u64,
         messages: HashSet<serde_json::Value>,
     },
-    DoGossip,
     Gossip {
         message: HashSet<serde_json::Value>,
     },
@@ -81,10 +82,14 @@ impl Body {
             Body::ReadOk { .. } => "read_ok",
             Body::Echo { .. } => "echo",
             Body::EchoOk { .. } => "echo_ok",
-            Body::DoGossip {} => "do_gossip",
             Body::Gossip { .. } => "gossip",
         }
     }
+}
+
+pub enum Event {
+    Message(Message),
+    Action(String),
 }
 pub enum MaelstromNodeId {
     Unassigned,
@@ -108,31 +113,34 @@ pub struct MaelstromNode<Extra> {
     pub node_id: MaelstromNodeId,
     pub next_send_id: u64,
     pub msg_handlers:
-        HashMap<&'static str, Rc<dyn Fn(&mut Self, Message) -> Option<Result<Message>>>>,
+        HashMap<&'static str, Rc<dyn Fn(&mut Self, Event) -> Option<Result<Message>>>>,
     pub extra_data: Extra,
 }
 
 pub trait MaelstromNodeActions<Extra: Default> {
     fn handle<F>(&mut self, msg_type: &'static str, handler: F)
     where
-        F: Fn(&mut Self, Message) -> Option<Result<Message>> + 'static;
-    fn process_msg(&mut self, msg: Message) -> Option<Result<Message>>;
+        F: Fn(&mut Self, Event) -> Option<Result<Message>> + 'static;
+    fn process_event(&mut self, event: Event) -> Option<Result<Message>>;
     fn send(&mut self, msg: Message) -> Result<()>;
-    fn run(&mut self) -> Result<()>;
+    fn run(&mut self, sender: Sender<Event>, receiver: Receiver<Event>) -> Result<()>;
 }
 
 impl<Extra: Default> MaelstromNodeActions<Extra> for MaelstromNode<Extra> {
     fn handle<F>(&mut self, msg_type: &'static str, handler: F)
     where
-        F: Fn(&mut Self, Message) -> Option<Result<Message>> + 'static,
+        F: Fn(&mut Self, Event) -> Option<Result<Message>> + 'static,
     {
         self.msg_handlers.insert(msg_type, Rc::new(handler));
     }
 
-    fn process_msg(&mut self, msg: Message) -> Option<Result<Message>> {
-        let name = msg.body.as_str();
-        match self.msg_handlers.get(name) {
-            Some(handler) => handler.clone()(self, msg),
+    fn process_event(&mut self, event: Event) -> Option<Result<Message>> {
+        let name = match &event {
+            Event::Action(action_name) => action_name,
+            Event::Message(msg) => msg.body.as_str(),
+        };
+        match self.msg_handlers.get(&name) {
+            Some(handler) => handler.clone()(self, event),
             None => None,
         }
     }
@@ -142,36 +150,53 @@ impl<Extra: Default> MaelstromNodeActions<Extra> for MaelstromNode<Extra> {
         Ok(())
     }
 
-    fn run(&mut self) -> Result<()> {
+    fn run(&mut self, sender: Sender<Event>, receiver: Receiver<Event>) -> Result<()> {
         self.handle(
             "init",
-            |node: &mut MaelstromNode<Extra>, msg: Message| -> Option<Result<Message>> {
-                if let Body::Init {
-                    node_id, msg_id, ..
-                } = msg.body
-                {
-                    node.node_id = node_id.into();
-                    Some(Ok(Message {
-                        src: node.node_id.as_str().expect("node id is not assigned"),
-                        dst: msg.src,
-                        body: Body::InitOk {
-                            in_reply_to: msg_id,
-                        },
-                    }))
-                } else {
-                    None
+            |node: &mut MaelstromNode<Extra>, event: Event| -> Option<Result<Message>> {
+                match event {
+                    Event::Message(msg) => {
+                        let Body::Init {
+                            node_id, msg_id, ..
+                        } = msg.body
+                        else {
+                            return None;
+                        };
+                        node.node_id = node_id.into();
+                        Some(Ok(Message {
+                            src: node.node_id.as_str().expect("node id is not assigned"),
+                            dst: msg.src,
+                            body: Body::InitOk {
+                                in_reply_to: msg_id,
+                            },
+                        }))
+                    }
+                    Event::Action(..) => None,
                 }
             },
         );
 
-        let stdin = std::io::stdin().lock();
-        for line in stdin.lines() {
-            let line = line?;
-            let req: Message = serde_json::from_str(&line)?;
-            if let Some(response) = self.process_msg(req) {
+        let jh = thread::spawn(move || -> Result<()> {
+            for line in std::io::stdin().lock().lines() {
+                let line = line.context("input from STDIN cannot be read")?;
+                let req: Message = serde_json::from_str(&line)?;
+                sender
+                    .send(Event::Message(req))
+                    .context("Error sending to sender in the STDIN thread")?;
+            }
+            Ok(())
+        });
+
+        for event in receiver {
+            if let Some(response) = self.process_event(event) {
                 self.send(response.unwrap())?;
             }
         }
-        Ok(())
+
+        jh.join()
+            .expect("STDIN thread panic")
+            .context("STDIN thread error out")?;
+
+        Ok::<(), anyhow::Error>(())
     }
 }
